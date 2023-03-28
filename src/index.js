da456283f79951be61ea265e37bd05869c5a8a9d
Aizen <iansky3311@gmail.com>
2023-03-28 22:27:23 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    const form = document.getElementById("create-task-form");
+    const taskInput = document.getElementById("new-task-description")
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        const task = taskInput.value;
+        const taskList =  document.getElementById("tasks");
+        const tasky = document.createElement("li");
+        tasky.innerText = task
+        const btn = document.createElement("button");
+        btn.innerText = "x";
+        tasky.appendChild(btn);
+        taskList.appendChild(tasky);
+        btn.addEventListener("click", function() {
+           tasky.remove();
+    });
+})
+})
